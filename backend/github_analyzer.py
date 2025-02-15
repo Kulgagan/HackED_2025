8da4621aa1a7fdad563f32e4415842df52c6c5bb
Kulgagan <Kulgagan08@gmail.com>
2025-02-15 13:00:34 -0700

    updates...

--- backend/github_analyzer.py
+++ backend/github_analyzer.py
@@ -1,1 +1,23 @@
-from github_analyzer import...
+from fastapi import FastAPI, HTTPException
+import requests 
+import os
+import git
+
+app = FastAPI()
+
+GITHUB_TOKEN = "find the token" #get ur token
+@app.post("/analyze")
+def analyze_repo(repo_url:str): 
+
+    #converting github url into api url
+    repo_name = repo_url.split("github.com/") [-1] 
+    api_url = f"https://api.github.com/repos/{repo_name}"
+
+    headers = {"Authorization": f"token {GITHUB_TOKEN}"}
+    response = requests.get(api_url, headers)
+
+    
+    if response.status_code != 200:
+        raise HTTPException(status_code = 404, detail = "repo not found") 
+    
+    repo_data = response.json()
