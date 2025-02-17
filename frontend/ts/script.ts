bf5ed41d8196ada8340150e49de8693c30051265
Hamoodi452 <adilsajid452@gmail.com>
2025-02-16 20:02:40 -0700

    update

--- frontend/ts/script.ts
+++ frontend/ts/script.ts
@@ -4,20 +4,4 @@ document.addEventListener("DOMContentLoaded", () => {
     const navButtons = document.getElementById("nav-buttons") as HTMLElement;
     const logo = document.getElementById("logo") as HTMLElement;
 
-    // Adjust the size of header, logo, and nav buttons on scroll
-    window.addEventListener("scroll", () => {
-        const scrollPosition = window.scrollY;
-
-        if (scrollPosition > 100) {
-            // Add shrink classes when scrolling down
-            banner.classList.add("shrink");
-            logo.classList.add("shrink-logo");
-            navButtons.classList.add("shrink-nav");
-        } else {
-            // Remove shrink classes when scrolling back to top
-            banner.classList.remove("shrink");
-            logo.classList.remove("shrink-logo");
-            navButtons.classList.remove("shrink-nav");
-        }
-    });
-});
+}
