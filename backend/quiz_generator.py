ab299957f16b1401dc75b4083cfa91312b80199e
Hammad-Zubair10 <Hammadzubair@hotmail.com>
2025-02-16 19:03:41 -0700

    update

--- backend/quiz_generator.py
+++ backend/quiz_generator.py
@@ -73,7 +73,51 @@ def generate_quiz(summary):
 
     quiz = []
     for i in range(len(questions)):
-        quiz.append({"question:": questions[i]})
+        quiz.append({"question:": questions[i], "options": options[i]})
+
+    return {"quiz": quiz, "total questions": len(questions)}
+
+
+'''
+API: Endpoint: Submit answers and get scores from the quiz
+
+'''
+@app.post("/submit-answers/")
+def submit_answers(user_answers: UserAnswers):
+
+    response = ollama.chat(
+        engine = 'llama3.1',
+        messages = [{"role": "system", "content": "You are a quiz generator "},
+                    {"role": "user", "content": f"Generate a  multiple choice practice quiz based on this code summary:\n\n"
+                                                  f"{Summary}"
+                                                  f"Provide exactly 5 multiple choice questions with 4 options (A, B, C, D). With the question starting with 'Q: '"
+                                                  f"After each question, state the correct answer in this format: \n"
+                                                  f"'Answer X: ' where X is A, B, C, or D."}]
+    )['messages']['content']
+
+    _, _, correct_answers = parse_quiz(response)
+
+    user_answers_list = user_answers.answers
+    score = 0
+
+    if len(user_answers.answers) != len(correct_answers):
+
+        raise HTTPException(status_code = 400, detail = "Not enough answers")
+    
+    i = 0
+    while i < len(correct_answers):
+        user_answer = user_answers_list[i].upper()
+        correct_answer = correct_answers[i]
+
+        if user_answer == correct_answer:
+            score += 1
+        
+        i += 1
+
+    return {"score": score, "total": len(correct_answers), "correct_answers": correct_answers, "user_answers": user_answers.answers}
+
+
+
 
 
 
